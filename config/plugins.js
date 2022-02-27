922fe5ac4c287f34acb2d4d48f45d92292d91b1b
victormmorales <victor.morales@criptan.com>
2022-02-27 22:09:32 +0100

    strapi upload configuration

--- config/plugins.js
+++ config/plugins.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   upload: {
     provider: "aws-s3",
     providerOptions: {
-      accessKeyId: "AKIAQ2DFCT753BDSF2GG",
-      secretAccessKey: "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",
-      region: "us-east-1",
+      accessKeyId: env("AWS_ACCESS_KEY_ID"),
+      secretAccessKey: env("AWS_ACCESS_SECRET"),
+      region: env("AWS_REGION"),
       params: {
-        Bucket: "game-ecommerce",
+        Bucket: env("AWS_BUCKET"),
       },
     },
   },
